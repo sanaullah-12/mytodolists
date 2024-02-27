9d2ca48af4f62295efef3070b0bbfff5c43a0998
sanaullah-12 <qazisanaullah612@gmail.com>
2024-02-27 18:12:28 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoList from './Component/TodoList/TodoList'
+export default function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  )
+}
+
